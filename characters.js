d22c80cce9d8879167792227e78c82e3745eba3d
WJStephenson <willjosteph@gmail.com>
2023-07-24 16:20:29 +0100

    Added no saved characters message

--- characters.js
+++ characters.js
@@ -3,38 +3,42 @@ const health = document.getElementById("health");
 
 // On page load get all saved characters from local storage and display them
 window.onload = function () {
-    for (let i = 0; i < localStorage.length; i++) {
-        const characters = localStorage.getItem(localStorage.key(i));
-        const character = JSON.parse(characters);
-        console.log(character);
-        const characterName = character.name;
-        const characterLevel = character.level;
-        const characterClass = character._class;
-        const characterRace = character.race;
-        const characterContainer = document.createElement("div");
-        const characterP = document.createElement("p");
-        characterContainer.classList.add("character");
-        const buttonContainer = document.createElement("div");
-        const loadButton = document.createElement("button");
-        const deleteButton = document.createElement("button");
-        loadButton.innerHTML = "Load";
-        deleteButton.innerHTML = "Delete";
-        loadButton.addEventListener("click", () => {
-            loadCharacter(character);
-        });
-        deleteButton.addEventListener("click", () => {
-            console.log(`Deleting`)
-            deleteCharacter(character.name);
-        });
+    if (localStorage.length === 0) {
+        container.innerHTML = "<p>No saved characters</p>";
+    } else {
+        for (let i = 0; i < localStorage.length; i++) {
+            const characters = localStorage.getItem(localStorage.key(i));
+            const character = JSON.parse(characters);
+            console.log(character);
+            const characterName = character.name;
+            const characterLevel = character.level;
+            const characterClass = character._class;
+            const characterRace = character.race;
+            const characterContainer = document.createElement("div");
+            const characterP = document.createElement("p");
+            characterContainer.classList.add("character");
+            const buttonContainer = document.createElement("div");
+            const loadButton = document.createElement("button");
+            const deleteButton = document.createElement("button");
+            loadButton.innerHTML = "Load";
+            deleteButton.innerHTML = "Delete";
+            loadButton.addEventListener("click", () => {
+                loadCharacter(character);
+            });
+            deleteButton.addEventListener("click", () => {
+                console.log(`Deleting`)
+                deleteCharacter(character.name);
+            });
 
-        characterP.innerHTML += `<div class="character-info">${characterName.charAt(0).toUpperCase() + characterName.slice(1)} is a level ${characterLevel} ${characterClass.charAt(0).toUpperCase() + characterClass.slice(1)} ${characterRace.charAt(0).toUpperCase() + characterRace.slice(1)}</div>`;
+            characterP.innerHTML += `<div class="character-info">${characterName.charAt(0).toUpperCase() + characterName.slice(1)} is a level ${characterLevel} ${characterClass.charAt(0).toUpperCase() + characterClass.slice(1)} ${characterRace.charAt(0).toUpperCase() + characterRace.slice(1)}</div>`;
 
 
-        buttonContainer.appendChild(deleteButton);
-        buttonContainer.appendChild(loadButton);
-        characterContainer.appendChild(buttonContainer);
-        characterContainer.appendChild(characterP);
-        container.appendChild(characterContainer);
+            buttonContainer.appendChild(deleteButton);
+            buttonContainer.appendChild(loadButton);
+            characterContainer.appendChild(buttonContainer);
+            characterContainer.appendChild(characterP);
+            container.appendChild(characterContainer);
+        }
     }
 };
 
